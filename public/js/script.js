8b51f9b62e09a845e043d255947dd4a6a767d029
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2024-09-25 23:23:49 +0530

    base

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+const socket = io();
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position) => {
+        const {latitude, longitude} = position.coords
+
+        socket.emit("sendLocation", {latitude, longitude})
+    }, (error) => {
+        console.log(error)
+    },
+    {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout:5000, 
+    }
+)
+}
+
+L.map("map").setView([0, 0], 10)
